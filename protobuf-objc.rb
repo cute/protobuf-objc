df9a77fb48dbd5608ab73bc4814ae10cf42ea7f9
Superbil <superbil@gmail.com>
2013-12-16 14:50:35 +0800

    remove no use options on use configure

--- protobuf-objc.rb
+++ protobuf-objc.rb
@@ -5,8 +5,7 @@ class ProtobufObjc < Formula
   url 'https://github.com/Superbil/protobuf-objc.git'
   sha1 '7dcd0d77c5aeb706510d67d157793fd689ad4073'
 
-  head 'https://github.com/Superbil/protobuf-objc.git',
-  :branch => 'develop'
+  head 'https://github.com/Superbil/protobuf-objc.git', :branch => 'develop'
 
   depends_on :autoconf
   depends_on :automake
@@ -17,9 +16,7 @@ class ProtobufObjc < Formula
   def install
     ENV.prepend 'CXXFLAGS', '-DNDEBUG'
     system "./autogen.sh"
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--with-zlib"
-    system "make", "install"
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "make install"
   end
 end
